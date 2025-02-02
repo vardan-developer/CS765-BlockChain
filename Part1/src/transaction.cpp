dd8d604e34415a0ba6e804c2dad0e276f2156752
Atharva Bendale <atharvabendale695@gmail.com>
2025-02-02 12:00:14 +0530

    Completed BlockTree implementation

--- Part1/src/transaction.cpp
+++ Part1/src/transaction.cpp
@@ -1,11 +1,11 @@
 #include "transaction.hpp"
 
 int Transaction::amount() const {
-    return std::accumulate(in_utxos.begin(), in_utxos.end(), 0, [](int sum, Utxo & utxo) { return sum + utxo.amount; });
+    return std::accumulate(out_utxos.begin(), out_utxos.end(), 0, [](int sum, Utxo & utxo) { return sum + utxo.amount; });
 }
 
 bool Transaction::isBalanceConsistent() const {
-    return ( type == TransactionType::COINBASE ) ||
+    return ( type == TransactionType::COINBASE && std::accumulate(out_utxos.begin(), out_utxos.end(), 0, [](int sum, Utxo & utxo) { return sum + utxo.amount; }) == 50) ||
         (   std::accumulate(in_utxos.begin(), in_utxos.end(), 0, [](int sum, Utxo & utxo) { return sum + utxo.amount; }) ==
             std::accumulate(out_utxos.begin(), out_utxos.end(), 0, [](int sum, Utxo & utxo) { return sum + utxo.amount; }));
 }
