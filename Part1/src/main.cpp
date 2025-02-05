195c6513746b3a2028ae616b6dce511d03925915
vardan-developer <vardan.verma.developer@gmail.com>
2025-02-05 17:17:18 +0530

    removed yml

--- Part1/src/main.cpp
+++ Part1/src/main.cpp
@@ -2,20 +2,22 @@
 #include "sim.hpp"
 #include "utils.hpp"
 
-void printNetworkTopology(std::vector<std::vector<std::pair<minerID_t, std::pair<int, int> > > > networkTopology);
+void printNetworkTopology(std::vector<std::vector<std::pair<minerID_t, std::pair<int, int>>>> networkTopology);
 
-std::vector<std::vector<std::pair<minerID_t, std::pair<int, int> > > > networkTopology;
+std::vector<std::vector<std::pair<minerID_t, std::pair<int, int>>>> networkTopology;
 std::set<minerID_t> highCPUMiners;
 // networkTopology[i][j] = (neighborID, (rho_ij, c_ij))
 // latency = rho_ij + |m| * 1000 / c_ij + d_ij -> in milliseconds
 // d_ij is randomly chosen from a exponential dist with mean 96*1000Kbits/c_ij (in milliseconds)
 // rho_ij is randomly chosen from a uniform dist between 10ms to 500ms
 // c_ij is 100*1024 Kbps if both i and j are fast else 5*1024 Kbps
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
     ProgramSettings settings;
     std::string errorMsg;
 
-    if(!parseArgs(argc, argv, settings, errorMsg)){
+    if (!parseArgs(argc, argv, settings, errorMsg))
+    {
         std::cerr << errorMsg << std::endl;
         return 1;
     }
@@ -26,10 +28,15 @@ int main(int argc, char* argv[]){
     simulator.run();
 }
 
-void printNetworkTopology(std::vector<std::vector<std::pair<minerID_t, std::pair<int, int> > > > networkTopology){
-    for(int i = 0; i < networkTopology.size(); i++){
+void printNetworkTopology(std::vector<std::vector<std::pair<minerID_t, std::pair<int, int>>>> networkTopology)
+{
+    for (int i = 0; i < networkTopology.size(); i++)
+    {
         std::cout << "Miner " << i << " has neighbors: ";
-        for(int j = 0; j < networkTopology[i].size(); j++){
+        for (int j = 0; j < networkTopology[i].size(); j++)
+        {
+            if (networkTopology[i][j].first == -1)continue;
             std::cout << networkTopology[i][j].first << " ";
         }
+    }
 }
