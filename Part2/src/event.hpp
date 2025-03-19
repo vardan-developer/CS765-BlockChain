cf8f6ad5e3b8f9642e3911d1a755936c449bcf3c
Atharva Bendale <atharvabendale695@gmail.com>
2025-03-19 16:36:44 +0530

    Modifed Event Class

--- Part2/src/event.hpp
+++ Part2/src/event.hpp
@@ -1,8 +1,11 @@
 #ifndef EVENT_H
 #define EVENT_H
 
+#include <ctime>
 #include <time.h>
 #include "block.hpp"
+#include "def.hpp"
+#include "miner.hpp"
 #include "transaction.hpp"
 
 /**
@@ -13,160 +16,213 @@
  * BLOCK_CREATION: Event for creating a new block
  * BROADCAST_BLOCK: Event for broadcasting a block
  * BROADCAST_TRANSACTION: Event for broadcasting a transaction
- * SEND_BROADCAST_BLOCK: Event for sending a broadcast block
- * SEND_BROADCAST_TRANSACTION: Event for sending a broadcast transaction
+ * SEND_BLOCK: Event for sending a broadcast block
+ * SEND_TRANSACTION: Event for sending a broadcast transaction
  */
+
 enum class EventType {
-    RECEIVE_BROADCAST_TRANSACTION,
-    RECEIVE_BROADCAST_BLOCK,
-    BLOCK_CREATION,
-    BROADCAST_BLOCK,
-    BROADCAST_TRANSACTION,
-    SEND_BROADCAST_BLOCK,
-    SEND_BROADCAST_TRANSACTION
+    SEND_TRANSACTION,
+    RECEIVE_TRANSACTION,
+    SEND_GET,
+    RECEIVE_GET,
+    SEND_HASH,
+    RECEIVE_HASH,
+    BROADCAST_PRIVATE_CHAIN
+};
+
+struct Event {
+    EventType type;
+    time_t timestamp;
+    minerID_t owner, sender, receiver;
+    bool broadcast, malicious;
+    
+    Event(EventType type, time_t timestamp, minerID_t owner, minerID_t sender, minerID_t receiver, bool broadcast, bool malicious):
+        type(type), timestamp(timestamp), owner(owner), sender(sender), receiver(receiver), broadcast(broadcast), malicious(malicious) {}
+
+    bool operator < (const Event& other) const {
+        return timestamp < other.timestamp;
+    }
+    bool operator == (const Event& other) const {
+        return timestamp == other.timestamp;
+    }
+    bool operator != (const Event& other) const {
+        return timestamp != other.timestamp;
+    }
+    bool operator > (const Event& other) const {
+        return timestamp > other.timestamp;
+    }
+};
+
+struct BlockEvent: Event {
+    Block block;
+    BlockEvent(EventType type, const Block block, time_t timestamp, minerID_t owner, minerID_t sender, minerID_t receiver, bool broadcast = false, bool malicious = false):
+        Event(type, timestamp, owner, sender, receiver, broadcast, malicious), block(block) {}
+};
+
+struct TransactionEvent: Event {
+    Transaction transaction;
+    TransactionEvent(EventType type, const Transaction transaction, time_t timestamp, minerID_t owner, minerID_t sender, minerID_t receiver, bool broadcast = false, bool malicious = false):
+        Event(type, timestamp, owner, sender, receiver, broadcast, malicious), transaction(transaction) {}
+};
+
+struct HashEvent: Event {
+    hash_t hash;
+    HashEvent(EventType type, const hash_t hash, time_t timestamp, minerID_t owner, minerID_t sender, minerID_t receiver, bool broadcast = false, bool malicious = false):
+        Event(type, timestamp, owner, sender, receiver, broadcast, malicious), hash(hash) {}
+};
+
+using GetEvent = HashEvent;
+
+struct BroadcastPrivateChainEvent: Event {
+    BroadcastPrivateChainEvent(EventType type, time_t timestamp, minerID_t owner, minerID_t sender, minerID_t receiver, bool broadcast = false, bool malicious = false):
+        Event(type, timestamp, owner, sender, receiver, broadcast, malicious) {}
 };
 
 /**
  * @brief Class representing events in the blockchain system
  * Contains either a Block or Transaction, but never both simultaneously
  */
-struct Event {
-    EventType type;          // Type of event (from EventType enum)
-    Block * block;          // Pointer to block (if block-related event)
-    Transaction * transaction; // Pointer to transaction (if transaction-related event)
-    time_t timestamp;       // Scheduled processing time for this event
-    minerID_t owner;        // ID of the miner who created this event
-    minerID_t receiver;     // ID of the intended receiving miner
+// struct Event {
+//     EventType type;          // Type of event (from EventType enum)
+//     Block * block;          // Pointer to block (if block-related event)
+//     Transaction * transaction; // Pointer to transaction (if transaction-related event)
+//     time_t timestamp;       // Scheduled processing time for this event
+//     minerID_t owner;        // ID of the miner who created this event
+//     minerID_t receiver;     // ID of the intended receiving miner
+//     bool broadcast;
+//     bool malicious;
+//     hash_t hash;
 
-    // Constructor for block-related events
-    // Creates a deep copy of the provided block
-    Event(EventType type, const Block* block, time_t timestamp, minerID_t owner, minerID_t receiver)
-        : type(type),
-          block(nullptr),
-          transaction(nullptr),
-          timestamp(timestamp),
-          owner(owner),
-          receiver(receiver)
-    {
-        this->block = new Block(*block);
-    }
+//     // Constructor for block-related events
+//     // Creates a deep copy of the provided block
+//     Event(EventType type, const Block* block, time_t timestamp, minerID_t owner, minerID_t receiver, bool broadcast = false, bool malicious = false)
+//         : type(type),
+//           block(new Block(*block)),
+//           transaction(nullptr),
+//           timestamp(timestamp),
+//           owner(owner),
+//           receiver(receiver),
+//           broadcast(broadcast),
+//           malicious(malicious) {}
 
-    // Constructor for transaction-related events
-    // Creates a deep copy of the provided transaction
-    Event(EventType type, const Transaction* transaction, time_t timestamp, minerID_t owner, minerID_t receiver)
-        : type(type),
-          block(nullptr),
-          timestamp(timestamp),
-          owner(owner),
-          receiver(receiver)
-    {
-        this->transaction = new Transaction(*transaction);
-    }
+//     // Constructor for transaction-related events
+//     // Creates a deep copy of the provided transaction
+//     Event(EventType type, const Transaction* transaction, time_t timestamp, minerID_t owner, minerID_t receiver, bool broadcast = false, bool malicious = false)
+//         : type(type),
+//           block(nullptr),
+//           transaction(new Transaction(*transaction)),
+//           timestamp(timestamp),
+//           owner(owner),
+//           receiver(receiver),
+//           broadcast(broadcast),
+//           malicious(malicious) {}
 
-    // Copy constructor
-    // Performs deep copy of either block or transaction
-    Event(const Event & other) {
-        type = other.type;
-        timestamp = other.timestamp;
-        owner = other.owner;
-        receiver = other.receiver;
-        if (other.block != nullptr) {
-            block = new Block(*other.block);
-            transaction = nullptr;
-        } else {
-            transaction = new Transaction(*other.transaction);
-            block = nullptr;
-        }
-    };
+//     Event(EventType type): type(type) {}
 
-    // Copy assignment operator
-    // Performs deep copy of either block or transaction
-    Event & operator = (const Event & other) {
-        if (this == &other) {
-            return *this;
-        }
-        type = other.type;
-        timestamp = other.timestamp;
-        owner = other.owner;
-        receiver = other.receiver;
-        if (other.block != nullptr) {
-            block = new Block(*other.block);
-            transaction = nullptr;
-        } else {
-            transaction = new Transaction(*other.transaction);
-            block = nullptr;
-        }
-        return *this;
-    }
+//     // Copy constructor
+//     // Performs deep copy of either block or transaction
+//     Event(const Event & other) {
+//         type = other.type;
+//         timestamp = other.timestamp;
+//         owner = other.owner;
+//         receiver = other.receiver;
+//         if (other.block != nullptr) {
+//             block = new Block(*other.block);
+//             transaction = nullptr;
+//         } else {
+//             transaction = new Transaction(*other.transaction);
+//             block = nullptr;
+//         }
+//     };
 
-    // Move constructor
-    // Transfers ownership of block or transaction
-    Event(Event && other) {
-        type = other.type;
-        timestamp = other.timestamp;
-        owner = other.owner;
-        receiver = other.receiver;
-        if (other.block != nullptr) {
-            this->block = other.block;
-            other.block = nullptr;
-            this->transaction = nullptr;
-        } else {
-            this->transaction = other.transaction;
-            other.transaction = nullptr;
-            this->block = nullptr;
-        }
-    }
+//     // Copy assignment operator
+//     // Performs deep copy of either block or transaction
+//     Event & operator = (const Event & other) {
+//         if (this == &other) {
+//             return *this;
+//         }
+//         type = other.type;
+//         timestamp = other.timestamp;
+//         owner = other.owner;
+//         receiver = other.receiver;
+//         if (other.block != nullptr) {
+//             block = new Block(*other.block);
+//             transaction = nullptr;
+//         } else {
+//             transaction = new Transaction(*other.transaction);
+//             block = nullptr;
+//         }
+//         return *this;
+//     }
 
-    // Move assignment operator
-    // Transfers ownership of block or transaction
-    Event & operator = (Event && other) {
-        if (this==&other) {
-            return *this;
-        }
-        type = other.type;
-        timestamp = other.timestamp;
-        owner = other.owner;
-        receiver = other.receiver;
-        if (other.block != nullptr) {
-            this->block = other.block;
-            other.block = nullptr;
-            this->transaction = nullptr;
-        } else {
-            this->transaction = other.transaction;
-            other.transaction = nullptr;
-            this->block = nullptr;
-        }
-        return *this;
-    }
+//     // Move constructor
+//     // Transfers ownership of block or transaction
+//     Event(Event && other) {
+//         type = other.type;
+//         timestamp = other.timestamp;
+//         owner = other.owner;
+//         receiver = other.receiver;
+//         if (other.block != nullptr) {
+//             this->block = other.block;
+//             other.block = nullptr;
+//             this->transaction = nullptr;
+//         } else {
+//             this->transaction = other.transaction;
+//             other.transaction = nullptr;
+//             this->block = nullptr;
+//         }
+//     }
 
-    // Comparison operators for event ordering based on timestamp
-    // Used for priority queue implementation
-    bool operator < (const Event& other) const {
-        return timestamp < other.timestamp;
-    }
+//     // Move assignment operator
+//     // Transfers ownership of block or transaction
+//     Event & operator = (Event && other) {
+//         if (this==&other) {
+//             return *this;
+//         }
+//         type = other.type;
+//         timestamp = other.timestamp;
+//         owner = other.owner;
+//         receiver = other.receiver;
+//         if (other.block != nullptr) {
+//             this->block = other.block;
+//             other.block = nullptr;
+//             this->transaction = nullptr;
+//         } else {
+//             this->transaction = other.transaction;
+//             other.transaction = nullptr;
+//             this->block = nullptr;
+//         }
+//         return *this;
+//     }
 
-    bool operator == (const Event& other) const {
-        return timestamp == other.timestamp;
-    }
+//     // Comparison operators for event ordering based on timestamp
+//     // Used for priority queue implementation
+//     bool operator < (const Event& other) const {
+//         return timestamp < other.timestamp;
+//     }
 
-    bool operator != (const Event& other) const {
-        return timestamp != other.timestamp;
-    }
+//     bool operator == (const Event& other) const {
+//         return timestamp == other.timestamp;
+//     }
 
-    bool operator > (const Event& other) const {
-        return timestamp > other.timestamp;
-    }
+//     bool operator != (const Event& other) const {
+//         return timestamp != other.timestamp;
+//     }
 
-    // Destructor
-    // Properly cleans up dynamically allocated block or transaction
-    ~Event() {
-        if ( block ) {
-            delete block;
-        }
-        if ( transaction ) {
-            delete transaction;
-        }
-    }
-};
+//     bool operator > (const Event& other) const {
+//         return timestamp > other.timestamp;
+//     }
+
+//     // Destructor
+//     // Properly cleans up dynamically allocated block or transaction
+//     ~Event() {
+//         if ( block ) {
+//             delete block;
+//         }
+//         if ( transaction ) {
+//             delete transaction;
+//         }
+//     }
+// };
 
 #endif
